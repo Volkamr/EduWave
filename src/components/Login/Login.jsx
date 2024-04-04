3df31fc711f33071d984e0345f79b561d3eee9ed
mpaulazzv <mariapaulaflorezvargas11@gmail.com>
2024-04-04 12:30:31 -0500

    Login v2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,9 +5,9 @@ import './Login.css'
 const Login = () => {
 
     const roles = [
-        { label: "Usuario", value: 'Usuario' },
-        { label: 'Administrador', value: 'Administrador' },
-        { label: 'Veterinario', value: 'Veterinario' }
+        { label: "Empleado", value: 'Empleado' },
+        { label: 'Instructor', value: 'Instructor' },
+        { label: 'Administrador', value: 'Administrador' }
     ]
 
     const [rol, setRol] = useState(roles[0]);
@@ -22,41 +22,48 @@ const Login = () => {
         <container className="login">
             <container className="login-container">
                 <div className="izquierda">
-                <h2 className="h2-login">Bienvenido a EduWave</h2>
-                <span></span>
+                        <h2 className="h2-izquierda">Bienvenido a EduWave</h2>
+                        <span className="l-izquierda"></span>
                 </div>
                 <div className="derecha">
-                <form >
+                    <form >
+                        <div className="titulo">
                             <h2 className="h2-login">Login</h2>
                             <span className="line"></span>
+                        </div>
 
-                            <div className="inputs">
-                                <p id="seleccionar-rol">Seleccione su rol</p>
-                                <Select id="select-r"
-                                    value={rol}
-                                    options={opcionesFiltradas}
-                                    onChange={handleSelectItem}
-                                >
-                                </Select>
+                        <div className="inputs">
+                            <label for="select-r">Seleccione su rol</label>
+                            <Select id="select-r"
+                                value={rol}
+                                options={opcionesFiltradas}
+                                onChange={handleSelectItem}
+                            >
+                            </Select>
 
-                                <label for="input-log" >Usuario</label>
-                                <input className="input-log" type="text"></input>
+                            <label for="input-log" >Usuario</label>
+                            <input className="input-log" type="text" name="usuario" required
+                                placeholder="Ingrese su usuario"></input>
 
 
-                                <label for="psswd">Contraseña</label>
-                                <input className="input-log" type="password" name="password" required
-                                    placeholder="Ingrese su contraseña" id="psswd"></input>
-        
+                            <label for="psswd">Contraseña</label>
+                            <input className="input-log" type="password" name="password" required
+                                placeholder="Ingrese su contraseña" id="psswd"></input>
 
-                                <input className="btn-login-reg btn-login-depth" type="submit" value="Ingresar"></input>
 
-                            </div>
-                        </form>
+                            <input className="btn-login-reg btn-login-depth" type="submit" value="Ingresar"></input>
+
+                            <p className="mensaje">
+                                    ¿Olvidó su contraseña?
+                                    <a className="a-login">Recuperar contraseña</a>
+                                </p>
+                        </div>
+                    </form>
 
                 </div>
             </container>
-    </container>
+        </container>
     );
-  };
-  
+};
+
 export default Login;
