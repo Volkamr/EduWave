e3eccb6d1ec7c9acb94f04d180b7ab8e82c7a698
LuzDD <marcela.vertel.rojas@gmail.com>
2024-05-22 11:44:07 -0500

    Guardar cambios locales en App.jsx antes de hacer pull

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Login from './components/Login/Login';
 import Usuario from './components/Usuario/Usuario';
 import PerfilUsuario from './components/PerfilUsuario/PerfilUsuario';
-
+import ForumPage from './components/ForumPage/ForumPage';
+import SideBar from './components/SideBar/SideBar';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Login/>}/>
         <Route path="/EduWave"element={<Usuario/>}/>
         <Route path="/EduWave/perfil" element={<PerfilUsuario/>}/>
+        <Route path="/EduWave/foros" element={<ForumPage />} />
+
       </Routes>
     </div>
   );
